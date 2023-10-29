048221895fcff3cab73d542cad588f36dde188a0
AdJansen <adamjansen3@gmail.com>
2023-10-29 19:23:11 -0400

    fixed null cookie error

--- interactive-constrained-clustering/src/components/pages/landingPage/inputForm/cookieManager/readCookies.js
+++ interactive-constrained-clustering/src/components/pages/landingPage/inputForm/cookieManager/readCookies.js
@@ -4,16 +4,18 @@ export function readCookies() {
     var pairs = document.cookie.split(";");
     var cookies = {};
 
-    var i = 0
-    for (; i<pairs.length-1; i++){
-      var pair = pairs[i].split("=");
-      cookies[(pair[0]+'').trim()] = pair.slice(1).join('=');
-    }
+    if (pairs.length > 1) {
+      var i = 0
+      for (; i<pairs.length-1; i++){
+        var pair = pairs[i].split("=");
+        cookies[(pair[0]+'').trim()] = pair.slice(1).join('=');
+      }
 
-    // handle the checked array seperately
-    var checked = pairs[i].split("=");
-    var checkedArray = checked[1].split(",");
-    cookies[(checked[0]+'').trim()] = checkedArray;
+      // handle the checked array seperately
+      var checked = pairs[i].split("=");
+      var checkedArray = checked[1].split(",");
+      cookies[(checked[0]+'').trim()] = checkedArray;
+    }
     
     return cookies;
 }
